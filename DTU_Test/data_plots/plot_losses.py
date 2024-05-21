a25ddfb8f1203ad565d5fe4e360e177553855b98
Ong <s204109@dtu.dk>
2024-05-21 16:02:18 +0200

    better loss plots

--- DTU_Test/data_plots/plot_losses.py
+++ DTU_Test/data_plots/plot_losses.py
@@ -12,29 +12,42 @@ all_train_losses = np.load(f'{folder_name}/all_train_losses.npy')
 all_train_metrics = np.load(f'{folder_name}/all_train_metrics.npy')
 all_val_metrics = np.load(f'{folder_name}/all_val_metric.npy')
 
-# Calculate stats
+# stats
 model_list = folder_name.split('/')[-1].split('-')[0].split('_')[:-1]
 num_models = len(model_list)
 num_epochs = len(all_train_losses)
 len_epoch = len(all_train_losses[0])
 
+fig, ax = plt.subplots(2,1, figsize=(12, 10))
+plt.tight_layout()
+plt.subplots_adjust(hspace=0.4)
+
 # Plot losses
 for model, name in enumerate(model_list):
     model_loss = all_individual_losses[:, :, model].flatten()
-    plt.plot(model_loss, label=f'{name}')
-
-# Plot ensemble (combined) loss
+    ax[0].plot(model_loss, label=f'{name}')
 ensemble_loss = all_train_losses.flatten()
-plt.plot(ensemble_loss, label='Ensemble', linestyle='--')
+ax[0].plot(ensemble_loss, label='Ensemble', linestyle='--')
+ax[0].set_xticks(np.arange(0, num_epochs*len_epoch, len_epoch))
+ax[0].set_xticklabels(np.arange(0, num_epochs))
+ax[0].set_xlim(0, num_epochs*len_epoch)
+ax[0].set_ylim(0)
+ax[0].grid()
+ax[0].legend()
+ax[0].set_title('Losses: TCL + GraphMixer (loss equally weighted)')
+ax[0].set_xlabel('n_Batch')
+ax[0].set_ylabel('Loss')
 
-# Plot epochs instead of batches
-plt.xticks(np.arange(0, num_epochs*len_epoch, len_epoch), np.arange(0, num_epochs))
+# Plot epoch mrr
+mrr = all_val_metrics
+ax[1].plot(mrr, label='MRR')
+ax[1].set_xticks(np.arange(0, num_epochs, 1))
+ax[1].set_xticklabels(np.arange(0, num_epochs))
+ax[1].set_xlim(0, num_epochs-1)
+ax[1].grid()
+ax[1].legend()
+ax[1].set_title('Validation MRR')
+ax[1].set_xlabel('Epoch')
+ax[1].set_ylabel('MRR')
 
-plt.xlim(0, num_epochs*len_epoch)
-plt.ylim(0)
-plt.grid()
-plt.legend()
-plt.title('Ensemble and Individual Losses')
-plt.xlabel('n_Batch')
-plt.ylabel('Loss')
 plt.show()
