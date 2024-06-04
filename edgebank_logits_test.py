3b99a55c9d304416b7854036552e5f0a126d3c78
danielries <s214641@dtu.dk>
2024-06-04 15:44:46 +0200

    frozen ensemble and edgebank logits

--- edgebank_logits_test.py
+++ edgebank_logits_test.py
@@ -17,6 +17,7 @@ from old_but_gold.modules.edgebank_predictor import EdgeBankPredictor
 from tgb.utils.utils import set_random_seed
 from tgb.linkproppred.dataset import LinkPropPredDataset
 from tgb.utils.utils import save_results
+from utils.metrics import get_link_prediction_metrics
 
 def ensemble_test(data, test_mask, neg_sampler, split_mode, subset='False', edgebank=None, metric=None):
     evaluator = Evaluator(name=DATA)
@@ -29,6 +30,8 @@ def ensemble_test(data, test_mask, neg_sampler, split_mode, subset='False', edge
     perf_list = []
     logits_list = []  # List to store logits for each batch
     test_labels = []  # List to store labels for each batch
+    pr_aucs = []
+    roc_aucs = []
 
     for batch_idx in tqdm(range(num_batches)):
         start_idx = batch_idx * BATCH_SIZE
@@ -56,13 +59,18 @@ def ensemble_test(data, test_mask, neg_sampler, split_mode, subset='False', edge
                 "eval_metric": [metric],
             }
             perf_list.append(evaluator.eval(input_dict)[metric])
-            
+            batch_metrics = get_link_prediction_metrics(torch.Tensor(y_pred), torch.Tensor(labels))
+            pr_aucs.append(batch_metrics['pr_auc'])
+            roc_aucs.append(batch_metrics['roc_auc'])
+
         # update edgebank memory after each positive batch
         edgebank.update_memory(pos_src, pos_dst, pos_t)
 
     perf_metrics = float(np.mean(perf_list))
+    pr_auc = float(np.mean(pr_aucs))
+    roc_auc = float(np.mean(roc_aucs))
 
-    return perf_metrics, logits_list, test_labels
+    return perf_metrics, pr_auc, roc_auc, logits_list, test_labels
 
 def get_edgebank_logits(subset, data):
     # data loading with `numpy`
@@ -92,21 +100,21 @@ def get_edgebank_logits(subset, data):
     dataset.load_test_ns()
 
     # testing ...
-    performance, logits, labels = ensemble_test(data, test_mask, neg_sampler, split_mode='test', subset=subset, edgebank=edgebank, metric=metric)
+    performance, pr_auc, roc_auc, logits, labels = ensemble_test(data, test_mask, neg_sampler, split_mode='test', subset=subset, edgebank=edgebank, metric=metric)
 
-    return performance, logits, labels
+    return performance, pr_auc, roc_auc, logits, labels
 
 def get_args_edgebank():
     parser = argparse.ArgumentParser('*** TGB: EdgeBank ***')
     parser.add_argument('--subset', type=str, help='Subset of the dataset', default='False', choices=['True', 'False'])
-    parser.add_argument('-d', '--data', type=str, help='Dataset name', default='tgbl-wiki', choices=['tgbl-coin', 'tgbl-comment', 'tgbl-flight', 'tgbl-review', 'tgbl-wiki'])
+    parser.add_argument('-d', '--data', type=str, help='Dataset name', default='tgbl-comment', choices=['tgbl-coin', 'tgbl-comment', 'tgbl-flight', 'tgbl-review', 'tgbl-wiki'])
     parser.add_argument('--run', type=str, help='Run name', default='run1')
     parser.add_argument('--k_value', type=int, help='k_value for computing ranking metrics', default=10)
     parser.add_argument('--seed', type=int, help='Random seed', default=1)
     parser.add_argument('--mem_mode', type=str, help='Memory mode', default='fixed_time_window', choices=['unlimited', 'fixed_time_window'])
     parser.add_argument('--time_window_ratio', type=float, help='Test window ratio', default=0.15)
     parser.add_argument('--bs', type=int, help='Batch size', default=1000)
-    parser.add_argument('--run_name', type=str, help='run_name', default="YAHNI")
+    parser.add_argument('--run_name', type=str, help='run_name', default="run_name")
 
     try:
         args_e = parser.parse_args()
@@ -128,12 +136,15 @@ SUBSET = args.subset
 #TIME_WINDOW_RATIO = args.time_window_ratio
 #run_name = args.run
 
-performance, logits, labels = get_edgebank_logits(subset=SUBSET, data=DATA)
+performance, pr_auc, roc_auc, logits, labels = get_edgebank_logits(subset=SUBSET, data=DATA)
 
 # print(labels)
 print(len(labels), labels[0].shape, type(logits),type(logits[0]))
 print(len(logits), logits[0].shape, type(logits),type(logits[0]))
-print(f"\nRESULTS: {performance}")
+print("\nRESULTS:")
+print(f"MRR: {performance}")
+print(f"PR AUC: {pr_auc}")
+print(f"ROC AUC: {roc_auc}")
 
 folder_name = f"./saved_results/EdgeBank/{args.data}/{args.run_name}/"
 os.makedirs(folder_name, exist_ok=True)
